661b4a68a19351cd9bcc751bdd885c9a3a7b41a5
Ben Keen <bkeen@itglue.com>
2020-08-23 18:01:14 -0700

    Updates

---
+++ precache-manifest.4f007b7d992197f62d84ff99620c8c61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0830bc9b48c40dcbf2f7b66c77029c29",
+    "url": "/checklist-generator/index.html"
+  },
+  {
+    "revision": "d7998a6911a73f94faff",
+    "url": "/checklist-generator/static/css/main.8790b243.chunk.css"
+  },
+  {
+    "revision": "27aaf7e3ec774d3635e5",
+    "url": "/checklist-generator/static/js/2.c08a31ff.chunk.js"
+  },
+  {
+    "revision": "260254c2b55f2376c2a0340f77dec2ca",
+    "url": "/checklist-generator/static/js/2.c08a31ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7998a6911a73f94faff",
+    "url": "/checklist-generator/static/js/main.ef7dec21.chunk.js"
+  },
+  {
+    "revision": "6bfd38c80db1741ecaef",
+    "url": "/checklist-generator/static/js/runtime-main.a7ba8a7d.js"
+  }
+]);
